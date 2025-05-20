13d0d31d5eb1bba6603449b04ebde7d8ec6a7a4b
gaddevstopps <Grant@sent4less.com>
2025-05-20 13:07:19 -0700

    Create AudioMergeApp.js

---
+++ src/AudioMergeApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AudioMergeApp.css';
+
+function AudioMergeApp() {
+  return (
+    <div className="container">
+      <h1>Audio Merge System</h1>
+      <p>This is a placeholder UI. Replace this with the real components for uploading CSVs, recording audio, and merging files.</p>
+    </div>
+  );
+}
+
+export default AudioMergeApp;
